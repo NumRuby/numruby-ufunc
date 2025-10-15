ad7824cee0418b4e3bc5ce98940c6e3b67120e5a
Shane Becker <veganstraightedge@gmail.com>
2025-10-15 04:08:46 -0700

    Rubocop

--- lib/numruby/ufunc.rb
+++ lib/numruby/ufunc.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "ufunc/version"
+require_relative 'ufunc/version'
 
 module Numruby
   module Ufunc
--- lib/numruby/ufunc/version.rb
+++ lib/numruby/ufunc/version.rb
@@ -2,6 +2,6 @@
 
 module Numruby
   module Ufunc
-    VERSION = "0.1.0"
+    VERSION = '0.1.0'
   end
 end
