05885ee0660aa725046e4b6ee78c7efb6344e168
Shane Becker <veganstraightedge@gmail.com>
2025-10-16 01:14:11 -0700

    Rubocop: Lint/RedundantDirGlobSort

--- lib/numruby/ufunc.rb
+++ lib/numruby/ufunc.rb
@@ -3,7 +3,7 @@ require_relative "ufunc/version"
 require_relative "ufunc/registry"
 
 # Dynamically require all ufunc definition files
-Dir[File.join(__dir__, "ufunc", "*.rb")].sort.each do |file|
+Dir[File.join(__dir__, "ufunc", "*.rb")].each do |file|
   next if file.end_with?("version.rb") || file.end_with?("registry.rb") || file.end_with?("delegators.rb")
 
   require_relative File.join("ufunc", File.basename(file))
