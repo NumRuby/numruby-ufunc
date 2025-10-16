0f7eaff4cd9513cc6475c2607808989a4d8d4a64
Shane Becker <veganstraightedge@gmail.com>
2025-10-16 01:48:49 -0700

    Fix require load paths, test .add()

--- lib/numruby-ufunc.rb
+++ lib/numruby-ufunc.rb
@@ -1,12 +1,7 @@
 require 'numo/narray'
-require_relative 'numruby/ufunc/version'
-require_relative 'numruby/ufunc/registry'
+require_relative 'numruby/ufunc'
 
 module NumRuby
   module UFunc
-    # Optional convenience method to mirror NumPy style
-    def self.call(name, *, **)
-      Registry.call(name, *, **)
-    end
   end
 end
--- spec/numruby/ufunc/add_spec.rb
+++ spec/numruby/ufunc/add_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 
 RSpec.describe NumRuby::UFunc::Add do
-  it 'adds two arrays correctly' do
-    x = Numo::DFloat[1, 2, 3]
-    y = Numo::DFloat[4, 5, 6]
-    result = described_class.call(x, y)
-    expect(result).to eq(Numo::DFloat[5, 7, 9])
+  describe '.call' do
+    subject { described_class.call(x, y) }
+
+    let(:x) { Numo::DFloat[1, 2, 3] }
+    let(:y) { Numo::DFloat[4, 5, 6] }
+
+    it 'adds two arrays correctly' do
+      expect(subject).to eq(Numo::DFloat[5, 7, 9])
+    end
+  end
+
+  describe '.add' do
+    subject { NumRuby.add(x, y) }
+
+    let(:x) { Numo::DFloat[1, 2, 3] }
+    let(:y) { Numo::DFloat[4, 5, 6] }
+
+    it 'adds two arrays correctly' do
+      expect(subject).to eq(Numo::DFloat[5, 7, 9])
+    end
   end
 end
